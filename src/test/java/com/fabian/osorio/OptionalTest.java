7b5e3c96ef04dc298a2bbfeaffb1ce5676df6627
Fabian Osorio <fadavidos@gmail.com>
2023-10-27 18:30:22 -0500

    added more unit test about Optional class

--- src/test/java/com/fabian/osorio/OptionalTest.java
+++ src/test/java/com/fabian/osorio/OptionalTest.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -81,9 +82,47 @@ public class OptionalTest {
 
         Function<Integer, Integer> duplicate = value -> value * 2;
 
-        assertEquals(6, optValue1.map(duplicate).orElse(999));
-        assertEquals(999, optValue2.map(duplicate).orElse(999));
+        assertEquals(Optional.of(6), optValue1.map(duplicate));
+        assertEquals(Optional.empty(), optValue2.map(duplicate));
     }
+
+    @Test
+    void flatMapFunction(){
+        Optional<Integer> optValue1 = Optional.ofNullable(3);
+        Optional<Integer> optValue2 = Optional.ofNullable(2);
+        Optional<Integer> optValue3 = Optional.empty();
+
+        Function<Integer, Optional<Integer>> duplicateIfEven = value ->
+                (value % 2 == 0) ? Optional.of(value * 2) : Optional.empty();
+
+        assertEquals(Optional.empty(), optValue1.flatMap(duplicateIfEven));
+        assertEquals(Optional.of(4), optValue2.flatMap(duplicateIfEven));
+        assertEquals(Optional.empty(), optValue3.flatMap(duplicateIfEven));
+    }
+
+    @Test
+    void orElseGetFunction(){
+        Optional<Integer> optValue1 = Optional.ofNullable(3);
+        Optional<Integer> optValue2 = Optional.empty();
+
+        Supplier<Integer> defaultValue = () -> 0;
+
+        assertEquals(3, optValue1.orElseGet(defaultValue));
+        assertEquals(0, optValue2.orElseGet(defaultValue));
+    }
+
+    @Test
+    void orElseThrowFunction(){
+        Optional<Integer> optValue1 = Optional.ofNullable(3);
+        Optional<Integer> optValue2 = Optional.empty();
+
+        Supplier<NoSuchElementException> defaultValue = () -> new NoSuchElementException("empty");
+
+        assertEquals(3, optValue1.orElseThrow(defaultValue));
+        assertThrows(NoSuchElementException.class, () -> optValue2.orElseThrow(defaultValue));
+    }
+
+
 
 
 }
